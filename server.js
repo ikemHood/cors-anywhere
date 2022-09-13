3ffc909e6479a67ec2ce64bda89a55650fd1c5d2
Ikem Peter <91676649+ikemHood@users.noreply.github.com>
2022-09-13 11:18:11 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: 'https://sailtest.netlify.app/,https://sailfi.co/,http://sailfi.co/',
+  originWhitelist: ['https://sailtest.netlify.app/','https://sailfi.co/','http://sailfi.co/'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
